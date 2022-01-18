2d4f6ec90d7db853773bf63b8c4c5a048d879aa6
astrixsanath14 <astrixsanath14@gmail.com>
2022-01-18 12:06:49 +0530

    Time: 43 ms (5.42%), Space: 20.3 MB (69.55%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -1,39 +1,45 @@
 class Solution {
 public:
-    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
-      int flowerbedSize = flowerbed.size();
-      if(flowerbedSize == 1)
-      {
-        return n-(flowerbed[0]?0:1) <= 0;
-      }
-      int prev=0;
-      int count=0;
-      for(int cur:flowerbed)
-      {
-        if(cur)
-          {
-           if(prev)
-            {
-             count--;
-            }
-          else
-          {
-            prev=1;
-          }
-          }
-          else
-          {
-            if(prev)
-            {
-              prev = 0;
-            }
-            else
-            {
-              prev = 1;
-              count++;
-            }
-          }
-      }
-      return count>=n;
+bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+
+    int len=flowerbed.size(), i=0, count=0; 
+    if(n==0)
+        return true;
+	// if flowerbed has 1 element only
+    if(len==1)
+        return (flowerbed[i]==1 ? false:true);
+		
+     // to check if flower can be planted in first plot
+    if(flowerbed[i]==0) {
+        if(flowerbed[i+1]==0) {
+            count++;
+            flowerbed[i]=1;
+        }
     }
+    i++;
+    
+    while(i<len-1) {
+        int flag=0;
+        if(flowerbed[i]==0) {
+            if(flowerbed[i-1]==0 && flowerbed[i+1]==0) {
+                count++;
+                flowerbed[i]=1;
+                i+=2;
+                flag=1;
+            }
+        }
+        
+        if(flag==0)
+          i++;
+    }
+    
+	// to check if flower can be planted in the last plot
+    if(flowerbed[len-1]==0 && flowerbed[len-2]==0) {
+        count++;
+        flowerbed[len-1]=1;
+    }
+    if(count>=n)
+        return true;
+return false;
+ }
 };
