0a252f26f03061b269b357ea32fb628c600f2ddc
astrixsanath14 <astrixsanath14@gmail.com>
2022-01-18 11:51:07 +0530

    Time: 231 ms (26.59%), Space: 14.5 MB (84.06%) - LeetHub

---
+++ 605-can-place-flowers/605-can-place-flowers.py
@@ -0,0 +1,17 @@
+class Solution:
+    def canPlaceFlowers(self, flowerbed: List[int], n: int) -> bool:
+        cnt, prev = 0, 0
+
+        for cur in flowerbed:
+            if cur == 1:
+                if prev == 1: # violation!
+                    cnt -= 1
+                prev = 1
+            else:
+                if prev == 1: # can't plant
+                    prev = 0 
+                else:
+                    cnt += 1
+                    prev = 1 # the cur plot gets taken
+            
+        return cnt >= n
