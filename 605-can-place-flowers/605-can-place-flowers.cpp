299605cbb92e095911baef0984996af87e52f6a3
astrixsanath14 <astrixsanath14@gmail.com>
2022-01-18 11:26:19 +0530

    Time: 23 ms (32.90%), Space: 20.2 MB (69.55%) - LeetHub

--- 605-can-place-flowers/605-can-place-flowers.cpp
+++ 605-can-place-flowers/605-can-place-flowers.cpp
@@ -5,12 +5,14 @@ public:
       flowerbed.push_back(0);
       for(int i = 1;i<flowerbed.size()-1;i++)
       {
-        if(!flowerbed[i] && !flowerbed[i-1] && !flowerbed[i+1])
+        if(!flowerbed[i] && !flowerbed[i-1] && !flowerbed[i+1] && n)
         {
           flowerbed[i] = 1;
           n--;
         }
-      }
-      return n<=0;
+        if(!n)
+          break;
+        }
+      return n==0;
     }
 };
