ccd476b2f8b22a08793719e463c8e795ef021380
astrixsanath14 <astrixsanath14@gmail.com>
2022-01-23 11:16:04 +0530

    Time: 233 ms (17.72%), Space: 93.3 MB (53.32%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.cpp
@@ -5,8 +5,8 @@ public:
       int profit = 0, highestSellingPrice = prices[prices.size() - 1];
       for(int i=prices.size()-2;i>=0;i--)
       {
-        highestSellingPrice = max(highestSellingPrice, prices[i]);
-        profit = max(profit, highestSellingPrice - prices[i]);
+        highestSellingPrice = highestSellingPrice>prices[i]?highestSellingPrice:prices[i];
+        profit = profit>highestSellingPrice - prices[i]?profit:highestSellingPrice - prices[i];
       }
       return profit;
     }
