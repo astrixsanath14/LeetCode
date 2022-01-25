dfe9db880282cf63677c45d4817c68ca449ffb2e
astrixsanath14 <astrixsanath14@gmail.com>
2022-01-25 10:08:55 +0530

    Time: 84 ms (5.17%), Space: 22.5 MB (48.39%) - LeetHub

---
+++ 941-valid-mountain-array/941-valid-mountain-array.cpp
@@ -0,0 +1,36 @@
+class Solution {
+public:
+    bool validMountainArray(vector<int>& arr) 
+    {
+      bool up=true;
+      int m=-1,size=arr.size();
+      if(size<3)
+        return false;
+      for(int i=1;i<size;i++)
+      {
+        cout<<up<<" "<<m<<" \n";
+        if(arr[i] > arr[i-1])
+        {
+          if(!up)
+            return false;
+          if(m==-1)
+            m=0;
+        }
+        else if(arr[i] < arr[i-1])
+        {
+          if(!m)
+          {
+            if(!up)
+              return false;
+            m=i;
+            up=false; 
+          }
+          else if(m==-1)
+            return false;
+        }
+        else
+          return false;
+      }
+      return !up;
+    }
+};
