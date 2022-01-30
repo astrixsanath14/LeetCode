047b0df30b0491ca2c07e07bab13ca508fc8fd7d
astrixsanath14 <astrixsanath14@gmail.com>
2022-01-30 11:28:46 +0530

    Time: 2258 ms (5.00%), Space: 27.7 MB (61.76%) - LeetHub

--- 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
+++ 84-largest-rectangle-in-histogram/84-largest-rectangle-in-histogram.py
@@ -11,8 +11,7 @@ class Solution(object):
             while heights[stack[-1]] > height:
                 h = heights[stack.pop()] 
                 w = i - stack[-1] - 1
-                print(h,w)
-                print(stack[-1])
+                print(i,stack[-1],h,w)
                 ans = max(ans, h*w)
             stack.append(i)
         heights.pop()
