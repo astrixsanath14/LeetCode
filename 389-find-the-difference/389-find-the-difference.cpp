7bba266b4ff50788698feb766ac5cbb0b1e2da5b
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-07 08:40:02 +0530

    Time: 0 ms (100.00%), Space: 6.5 MB (98.61%) - LeetHub

--- 389-find-the-difference/389-find-the-difference.cpp
+++ 389-find-the-difference/389-find-the-difference.cpp
@@ -2,9 +2,8 @@ class Solution {
 public:
     char findTheDifference(string s, string t) 
     {
-      long diff = 0;
       for(int i=0;i<s.size();i++)
-        diff+=t[i]-s[i];
-          return diff + t[t.size()-1];
+        t[i+1]+=t[i]-s[i];
+      return t[t.size()-1];
     }
 };
