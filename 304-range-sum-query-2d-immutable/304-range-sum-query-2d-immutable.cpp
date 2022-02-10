6f7ae997d1d09a044b1619f71052f5d2494ed467
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-11 00:15:54 +0530

    Time: 603 ms (49.02%), Space: 153.4 MB (6.96%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,74 @@
+class NumMatrix {
+public:
+  vector<vector<long long>>prefixMatrix;
+  vector<vector<int>>originalMatrix;
+    NumMatrix(vector<vector<int>>& matrix) {
+      originalMatrix = matrix;
+      int m = matrix.size();
+      int n = matrix[0].size();
+      prefixMatrix.resize(m);
+      for (int i = 0; i < m; ++i)
+      {
+        prefixMatrix[i].resize(n);
+        for(int j=0;j<n;j++)
+        {
+          prefixMatrix[i][j]=matrix[i][j];
+        }
+      }
+      for(int i=0;i<m;i++)
+      {
+        for(int j=1;j<n;j++)
+        {
+          prefixMatrix[i][j]+=prefixMatrix[i][j-1];
+        }
+      }
+      for(int i=0;i<n;i++)
+      {
+        for(int j=1;j<m;j++)
+        {
+          prefixMatrix[j][i]+=prefixMatrix[j-1][i];
+        }
+      }
+      
+      // for(int i=0;i<m;i++)
+      // {
+      //   for(int j=0;j<n;j++)
+      //   {
+      //     cout<<prefixMatrix[i][j]<<" ";
+      //   }
+      //   cout<<"\n";
+      // }
+      // cout<<"-----\n";
+      // for(int i=0;i<m;i++)
+      // {
+      //   for(int j=0;j<n;j++)
+      //   {
+      //     cout<<originalMatrix[i][j]<<" ";
+      //   }
+      //   cout<<"\n";
+      // }
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        long long sum =  prefixMatrix[row2][col2];
+        if(row1>0)
+        {
+          sum-=prefixMatrix[row1-1][col2];
+        }
+      if(col1>0)
+        {
+          sum-=prefixMatrix[row2][col1-1];
+        }
+      if(row1>0 && col1>0)
+      {
+        sum+=prefixMatrix[row1-1][col1-1];
+      }
+      return sum;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
