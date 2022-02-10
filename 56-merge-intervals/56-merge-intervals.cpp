51f551605f249a384b4d1e188b5f808e2a0834eb
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-10 22:34:20 +0530

    Time: 28 ms (63.46%), Space: 19 MB (41.34%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.cpp
+++ 56-merge-intervals/56-merge-intervals.cpp
@@ -2,8 +2,8 @@ class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
       int n = intervals.size();
-      sort(intervals.begin(), intervals.end());
-      // sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int>  &b) -> bool{return a[0] <= b[0];});
+      // sort(intervals.begin(), intervals.end());
+      sort(intervals.begin(), intervals.end(), [](const vector<int> &a, const vector<int>  &b) -> bool{return a[0] < b[0];});
       vector<vector<int>> ans;
       vector<int>cur{intervals[0][0], intervals[0][1]};
       for(int i=1;i<n;i++)
