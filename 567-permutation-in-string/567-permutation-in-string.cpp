0798388777daceb9dc8c785dc13dbc3b17f08858
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-12 01:21:35 +0530

    Time: 3 ms (98.40%), Space: 7.3 MB (71.47%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.cpp
+++ 567-permutation-in-string/567-permutation-in-string.cpp
@@ -1,40 +1,16 @@
 class Solution {
 public:
-  bool hasKey(unordered_map<char, int> m, char key)
-  {
-      return m.count(key) != 0;
-  }
-  bool checkInclusion(string s1, string s2) {
-    int k=0, matched=0, s1Len = s1.size();
-    unordered_map<char,int> counter1,counter2;
-    for(int i=0;i<s1Len;i++)
-    {
-      if(!hasKey(counter1, s1[i])){
-        counter1[s1[i]] = 0;
-        counter2[s1[i]] = 0;
-        k++;
-      }
-      counter1[s1[i]]++;
-    }
-    for(int i=0;i<s2.size();i++){
-      if(i>=s1Len)
-      {
-        if(hasKey(counter2, s2[i-s1Len]))
-        {
-          if(counter1[s2[i-s1Len]] == counter2[s2[i-s1Len]])
-            matched--;
-          counter2[s2[i-s1Len]]--;
+    bool checkInclusion(string s1, string s2) {
+        int map[26] = {0};
+        for(char c : s1) map[c - 'a']++;
+        int j = 0, i = 0, count_chars = s1.size();
+        while(j < s2.size()){
+            if(map[s2.at(j++) - 'a']-- > 0)
+                count_chars--;
+            if(count_chars == 0) return true;
+            if(j - i == s1.size() && map[s2.at(i++) - 'a']++ >= 0)
+                count_chars++;
         }
-      }
-      if(hasKey(counter2,s2[i]))
-      {
-        counter2[s2[i]]++;
-        if(counter1[s2[i]] == counter2[s2[i]])
-          matched++;
-        if(matched == k)
-          return true;
-      }
+        return false;
     }
-    return false;
-  }
 };
