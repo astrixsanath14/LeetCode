1f6814f5aeb7162bb0bb301b19777c7b7194e307
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-12 10:03:18 +0530

    Time: 24 ms (76.31%), Space: 9.8 MB (99.54%) - LeetHub

--- 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
+++ 1456-maximum-number-of-vowels-in-a-substring-of-given-length/1456-maximum-number-of-vowels-in-a-substring-of-given-length.cpp
@@ -5,14 +5,16 @@ public:
     return c=='a'||c=='e'||c=='i'||c=='o'||c=='u';
   }
   int maxVowels(string s, int k) {
-    int count = 0, maxCount = 0, n=s.length();
-    for(int i=0;i<n;i++)
+    int count = 0, maxCount = 0;
+    for(int i=0;i<s.length();i++)
     {
       if(isVowel(s[i]))
         count++;
       if(i>=k && isVowel(s[i-k]))
         count--;
       maxCount = max(maxCount, count);
+      if(maxCount == k)
+        return k;
     }
     return maxCount;
   }
