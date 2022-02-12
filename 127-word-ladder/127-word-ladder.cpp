5e6c32c8571f28a6965a22b6d40e1a4dc2af9db1
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-12 13:20:16 +0530

    Time: 1269 ms (15.66%), Space: 138.3 MB (5.01%) - LeetHub

--- 127-word-ladder/127-word-ladder.cpp
+++ 127-word-ladder/127-word-ladder.cpp
@@ -1,3 +1,4 @@
+unordered_map<string,bool> wordOccurrence;
 class Graph {
   int numVertices;
   map<string, list<string>> adjLists;
@@ -9,7 +10,7 @@ class Graph {
   void setVertices(vector<string> &vertices);
   void addVertex(string &vertex);
   void addEdge(string &src, string &dest);
-  int BFS(string &startVertex, string &endVertex); 
+  int BFS(string &startVertex, string &endVertex, vector<string>& wordList);
 };
 
 // Create a graph with given vertices,
@@ -40,7 +41,7 @@ void Graph::addVertex(string &vertex)
 }
 
 // BFS algorithm
-int Graph::BFS(string &startVertex, string &endVertex) {
+int Graph::BFS(string &startVertex, string &endVertex, vector<string>& wordList) {
   list<string> queue;
   queue.push_back(startVertex);
   
@@ -48,74 +49,82 @@ int Graph::BFS(string &startVertex, string &endVertex) {
   
   list<string>::iterator i;
   vertexDistance[startVertex] = 1;
+  int len = wordList[0].length();
   while (!queue.empty()) {
     string currVertex = queue.front();
     queue.pop_front();
-    for (i = adjLists[currVertex].begin(); i != adjLists[currVertex].end(); ++i) {
-      string adjVertex = *i;
-      if (!visited[adjVertex]) {
-        vertexDistance[adjVertex] = vertexDistance[currVertex]+1;
-        visited[adjVertex] = true;
-        queue.push_back(adjVertex);
-        if(adjVertex == endVertex)
+    
+    //Modified
+    for(int i=0;i<len;i++)
+    {
+      for(int j=0;j<26;j++)
+      {
+        string temp = currVertex;
+        temp[i] = 'a' + j;
+        if(temp!=currVertex && wordOccurrence[temp])
         {
-          return vertexDistance[adjVertex];
+          string adjVertex = temp;
+          if (!visited[adjVertex]) {
+            vertexDistance[adjVertex] = vertexDistance[currVertex]+1;
+            visited[adjVertex] = true;
+            queue.push_back(adjVertex);
+            if(adjVertex == endVertex)
+            {
+              return vertexDistance[adjVertex];
+            }
+          }
         }
       }
     }
+    
+    // for (i = adjLists[currVertex].begin(); i != adjLists[currVertex].end(); ++i) {
+    //   string adjVertex = *i;
+    //   if (!visited[adjVertex]) {
+    //     vertexDistance[adjVertex] = vertexDistance[currVertex]+1;
+    //     visited[adjVertex] = true;
+    //     queue.push_back(adjVertex);
+    //     if(adjVertex == endVertex)
+    //     {
+    //       return vertexDistance[adjVertex];
+    //     }
+    //   }
+    // }
   }
   return 0;
 }
 
 class Solution {
 public:
-  unordered_map<string,bool> wordOccurrence;
   
   int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
+    wordOccurrence.clear();
     for(auto word:wordList)
       wordOccurrence[word] = true;
-    wordOccurrence[beginWord] = true;
     if(!wordOccurrence[endWord])
       return 0;
     int len = wordList[0].length();
     Graph g(wordList.size()+1);
     g.setVertices(wordList);
     g.addVertex(beginWord);
-    for(auto word:wordList)
-    {
-      vector<string> adjacentWords{word};
-      for(int i=0;i<len;i++)
-      {
-        for(int j=0;j<26;j++)
-        {
-          string temp = word;
-          temp[i] = 'a' + j;
-          if(temp!=word && wordOccurrence[temp])
-          {
-            g.addEdge(word, temp);
-          }
-        }
-      }
-    }
-    if(!count(wordList.begin(), wordList.end(), beginWord))
-    {
-      bool foundAtLeastOneSeq = false;
-      for(int i=0;i<len;i++)
-      {
-        for(int j=0;j<=26;j++)
-        {
-          string temp = beginWord;
-          temp[i] = 'a'+j;
-          if(temp != beginWord && wordOccurrence[temp])
-          {
-            foundAtLeastOneSeq = true;
-            g.addEdge(beginWord, temp);
-          }
-        }
-      }
-      if(!foundAtLeastOneSeq)
-        return 0;
-    }
-    return g.BFS(beginWord, endWord);
+    // if(!wordOccurrence[beginWord])
+    // {
+    //   bool foundAtLeastOneSeq = false;
+    //   for(int i=0;i<len;i++)
+    //   {
+    //     for(int j=0;j<=26;j++)
+    //     {
+    //       string temp = beginWord;
+    //       temp[i] = 'a'+j;
+    //       if(temp != beginWord && wordOccurrence[temp])
+    //       {
+    //         foundAtLeastOneSeq = true;
+    //         g.addEdge(beginWord, temp);
+    //       }
+    //     }
+    //   }
+    //   if(!foundAtLeastOneSeq)
+    //     return 0;
+    // }
+    return g.BFS(beginWord, endWord, wordList);
   }
 };
