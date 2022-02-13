1e61f5fa76b1d483ce19c95ccadefeeb7f83422b
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-13 15:01:23 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (91.69%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+  vector<vector<int>> generate(int numRows) {
+    vector<vector<int>> pascalTriangle;
+    for(int i=0;i<numRows;i++)
+    {
+      int colCount = i+1;
+      vector<int> temp(colCount, 1);
+      for(int j=1;j<colCount-1;j++)
+      {
+        temp[j] = pascalTriangle[i-1][j-1] + pascalTriangle[i-1][j];
+      }
+      pascalTriangle.push_back(temp);
+    }
+    return pascalTriangle;
+  }
+};
