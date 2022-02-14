ca80a8499689d8d287cb0c3a2322004ee24476c0
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-14 09:11:57 +0530

    Time: 32 ms (40.04%), Space: 8.5 MB (47.04%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -5,19 +5,13 @@ public:
     int maxLen = 0, counter=0, first = 0;
     for(int i=0;i<s.length();i++)
     {
-      if(!posTracker.count(s[i]))
+      if(!posTracker.count(s[i]) || posTracker[s[i]]<first)
         posTracker[s[i]]= -1;
       counter++;
       if(posTracker[s[i]] != -1)
       {
         counter -= posTracker[s[i]] - first + 1;
         first = posTracker[s[i]]+1;
-        for (auto character : posTracker)
-        {
-          if(character.second < first){
-            posTracker[character.first] = -1;
-          }
-        }
       }
       posTracker[s[i]] = i;
       maxLen = max(maxLen, counter);
