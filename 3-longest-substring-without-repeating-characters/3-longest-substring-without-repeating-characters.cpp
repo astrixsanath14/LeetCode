5fb1662ea66061e986993d60e44fe863830d2769
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-14 09:22:26 +0530

    Time: 26 ms (48.21%), Space: 8.3 MB (72.22%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -2,20 +2,16 @@ class Solution {
 public:
   int lengthOfLongestSubstring(string s) {
     unordered_map<char,int> posTracker;
-    int maxLen = 0, counter=0, first = 0;
+    int maxLen = 0, first = 0;
     for(int i=0;i<s.length();i++)
     {
       if(!posTracker.count(s[i]) || posTracker[s[i]]<first)
         posTracker[s[i]]= -1;
-      counter++;
       if(posTracker[s[i]] != -1)
-      {
-        counter -= posTracker[s[i]] - first + 1;
         first = posTracker[s[i]]+1;
-      }
       posTracker[s[i]] = i;
-      maxLen = max(maxLen, counter);
-      if(s.length() - i - 1 + counter <= maxLen)
+      maxLen = max(maxLen, i-first+1);
+      if(s.length() - first <= maxLen)
         return maxLen;
     }
     return maxLen;
