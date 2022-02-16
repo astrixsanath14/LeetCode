fc7f2dffe7dd4a41f23994cefbbb267676f3e76b
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-17 03:41:11 +0530

    Time: 2 ms (77.23%), Space: 5.8 MB (92.29%) - LeetHub

--- 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
+++ 1545-find-kth-bit-in-nth-binary-string/1545-find-kth-bit-in-nth-binary-string.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
-  int getNextK(int n, int k){
-    long len = pow(2,n), mid = len/2;
-    
-    cout<<n<<" "<<k<<" "<<len<<" "<<mid<<"\n";
-    if(mid == k)
-      return -1;
-    if(k>mid)
-      k = mid - (k - mid);
-    return k;
-  }
-  char findKthBit(int n, int k) {
-    if(n == 1)
-      return '0';
-    int nextK = getNextK(n, k);
-    if(nextK == -1)
-      return '1';
-    if(k!=nextK){
-      return '1' - findKthBit(n-1,nextK) + '0';
+    char func(int n,int k)
+    {
+      cout<<n<<" "<<k<<"\n";
+        if(n==1)
+            return '0';
+        long mid=(pow(2,n))/2;
+        if(mid==k) 
+            return '1';
+        else if(k<mid) 
+            return func(n-1,k);
+        else 
+            return '1' - func(n-1,pow(2,n)-k) + '0';
     }
-    return findKthBit(n-1,nextK);
-  }
+    char findKthBit(int n, int k) 
+    {
+       return func(n,k);
+    }
 };
