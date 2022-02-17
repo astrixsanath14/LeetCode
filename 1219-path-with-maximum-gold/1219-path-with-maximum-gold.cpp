00399dd3fd0937cf89aa4eb30f1565df15d37b23
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-17 23:28:13 +0530

    Time: 208 ms (15.93%), Space: 7.5 MB (29.43%) - LeetHub

---
+++ 1219-path-with-maximum-gold/1219-path-with-maximum-gold.cpp
@@ -0,0 +1,28 @@
+
+int dx[] = {1,0,-1,0};
+int dy[] = {0,1,0,-1};
+class Solution {
+public:
+  int getMaximumGold(vector<vector<int>>& grid, int currI, int currJ, int &rows, int &columns)
+  {
+    if(currI<0 || currI>=rows || currJ<0 || currJ>=columns || grid[currI][currJ]<=0)
+      return 0;
+    int maxGold = 0;
+    grid[currI][currJ]*=-1;
+    
+    for(int p=0;p<4;p++)
+      maxGold = max(maxGold, getMaximumGold(grid, currI + dx[p], currJ + dy[p], rows, columns));
+    
+    grid[currI][currJ]*=-1;
+    return maxGold + grid[currI][currJ];
+  }
+  
+  int getMaximumGold(vector<vector<int>>& grid) {
+   int maxGold = 0, m = grid.size(), n = grid[0].size();
+    for(int i=0;i<m;i++)
+      for(int j=0;j<n;j++)
+        if(grid[i][j]>0)
+          maxGold = max(maxGold, getMaximumGold(grid, i, j, m, n));
+    return maxGold;
+  }
+};
