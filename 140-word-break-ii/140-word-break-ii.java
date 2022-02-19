efb386edccd11ca3e75b3867edb341eb0ff793e5
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-20 00:11:26 +0530

    Time: 11 ms (9.86%), Space: 42.6 MB (13.72%) - LeetHub

---
+++ 140-word-break-ii/140-word-break-ii.java
@@ -0,0 +1,40 @@
+class Solution {
+  public void solve(String s, int startIdx, int currIdx, String sentence, Set<String> wordDict, List<String> res)
+  {
+    if(currIdx == s.length())
+    {
+      if(startIdx < currIdx)
+      {
+        String currWord = s.substring(startIdx, currIdx);
+        if(wordDict.contains(currWord))
+        {
+          if(sentence!="")
+            sentence += " ";
+          sentence += currWord;
+        }
+        else
+          return;
+      }
+      res.add(sentence);
+      return;
+    }
+    String currWord = s.substring(startIdx, currIdx);
+    System.out.println("currWord: " + currWord);
+    if(wordDict.contains(currWord))
+    {
+      String nextSentence = sentence;
+      if(nextSentence!="")
+        nextSentence += " ";
+      nextSentence += currWord;
+      solve(s, currIdx, currIdx+1, nextSentence, wordDict, res);
+    }
+    solve(s, startIdx, currIdx+1, sentence, wordDict, res);
+  }
+  
+  public List<String> wordBreak(String s, List<String> wordDict) {
+    List<String> res = new ArrayList();
+    Set<String> mp = new HashSet(wordDict);
+    solve(s, 0, 0, "", mp, res);
+    return res;
+  }
+}
