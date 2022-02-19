8f90cfe574a3e4c213f546ea39a4b460abd6ae81
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-19 21:54:36 +0530

    Time: 0 ms (100.00%), Space: 8.1 MB (90.82%) - LeetHub

--- 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
+++ 1038-binary-search-tree-to-greater-sum-tree/1038-binary-search-tree-to-greater-sum-tree.cpp
@@ -9,21 +9,20 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-
-void reverseInOrderTraversal(TreeNode* root, int& sum){
-  if(!root)
-    return;
-  reverseInOrderTraversal(root->right, sum);
-  sum+= root->val;
-  root->val = sum;
-  reverseInOrderTraversal(root->left, sum);
-}
-
 class Solution {
-public:
-  TreeNode* bstToGst(TreeNode* root) {
-    int sum=0;
-    reverseInOrderTraversal(root, sum);
-    return root;
+  void reverseInOrderTraversal(TreeNode* root, int& sum){
+    if(!root)
+      return;
+    reverseInOrderTraversal(root->right, sum);
+    sum+= root->val;
+    root->val = sum;
+    reverseInOrderTraversal(root->left, sum);
   }
+
+  public:
+    TreeNode* bstToGst(TreeNode* root) {
+      int sum=0;
+      reverseInOrderTraversal(root, sum);
+      return root;
+    }
 };
