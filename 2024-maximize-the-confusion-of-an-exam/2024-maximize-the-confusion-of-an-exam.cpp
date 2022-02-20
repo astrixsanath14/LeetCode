0e9590b17a243a782939c19215768ed5899e19fc
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-20 17:22:51 +0530

    Time: 105 ms (10.01%), Space: 9.9 MB (93.88%) - LeetHub

---
+++ 2024-maximize-the-confusion-of-an-exam/2024-maximize-the-confusion-of-an-exam.cpp
@@ -0,0 +1,44 @@
+class Solution {
+public:
+  bool isPosibleConsecutiveAnswers(string &answerKey, int k, int l)
+  {
+    if(l > answerKey.length())
+      return false;
+    int tCount = 0;
+    for(int i=0;i<l;i++)
+      tCount += answerKey[i] == 'T';
+    if(tCount <= k || l-tCount <= k)
+      return true;
+    
+    for(int i=l;i<answerKey.length();i++)
+    {
+      tCount -= answerKey[i-l] == 'T';
+      tCount += answerKey[i] == 'T';
+      if(tCount <= k || l-tCount <= k)
+        return true;
+    }
+    return false;
+  }
+  
+  int maxConsecutiveAnswers(string answerKey, int k) {
+    int left = 1, right = answerKey.length(), maxPossibleLen = -1;
+    while(left <= right)
+    {
+      int mid = (left+right)/2;
+      bool isPossible = isPosibleConsecutiveAnswers(answerKey, k, mid);
+      cout<<"left: "<<left<<" right: "<<right<<" mid: "<<mid<<" isPossible: "<<boolalpha<<isPossible<<"\n";
+      if(!isPossible)
+      {
+        right = mid-1;
+      }
+      else
+      {
+        maxPossibleLen = mid;
+        if(!isPosibleConsecutiveAnswers(answerKey, k, mid+1))
+          return mid;
+        left = mid + 1;
+      }
+    }
+    return maxPossibleLen;
+  }
+};
