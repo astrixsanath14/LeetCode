0b7c45cdcea208184e26d456bf513201832539e3
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-24 23:35:59 +0530

    Time: 1215 ms (5.02%), Space: 29.1 MB (73.00%) - LeetHub

---
+++ 148-sort-list/148-sort-list.cpp
@@ -0,0 +1,77 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+
+void printLinkedList(ListNode* head)
+{
+  while(head!=NULL){
+    cout<<head->val<<" ";
+    head=head->next;
+  }
+  cout<<"\n";
+}
+
+class Solution {
+public:
+  ListNode* sortList(ListNode* head) {
+    if(head == NULL || head->next==NULL)
+      return head;
+    ListNode* it = head;
+    int prevVal = it->val;
+    ListNode* prev = it;
+    while(it!=NULL)
+    {
+      cout<<"Here \n";
+      cout<<prev->val<<" "<<it->val<<"\n";
+      while(it!=NULL && it->val>=prevVal){
+        prevVal = it->val;
+        prev=it;
+        it=it->next;
+      }
+      if(it == NULL)
+        break;
+      
+      ListNode* moveNode = it;
+      prev->next=moveNode->next;
+      it=it->next;
+      cout<<"Shift "<<moveNode->val<<"\n";
+      cout<<"Head "<<head->val<<"\n";
+      if(moveNode->val<=head->val)
+      {
+        moveNode->next=head;
+        head=moveNode;
+      }
+      else
+      {
+        ListNode* leftNode=head;
+        ListNode* rightNode = leftNode->next;
+        while(rightNode!=NULL && moveNode->val>rightNode->val)
+        {
+          leftNode=rightNode;
+          rightNode=rightNode->next;
+        }
+        leftNode->next = moveNode;
+        moveNode->next = rightNode;
+        cout<<"Done \n";
+      }
+      if(it==NULL)
+        break;
+    }
+  printLinkedList(head);
+  return head;
+  }
+};
+
+/*
+1 2 3 4 - sorted
+
+1 2 4 3
+
+*/
