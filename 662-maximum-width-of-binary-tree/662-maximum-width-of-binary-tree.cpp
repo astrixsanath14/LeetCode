509d7d2db6f03d24b2de2425880fcd9bb20f0129
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-27 23:11:57 +0530

    Time: 8 ms (85.32%), Space: 17.5 MB (21.15%) - LeetHub

--- 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
+++ 662-maximum-width-of-binary-tree/662-maximum-width-of-binary-tree.cpp
@@ -1,3 +1,5 @@
+#define ull long long int
+
 /**
  * Definition for a binary tree node.
  * struct TreeNode {
@@ -9,62 +11,64 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-// class Solution {
-// public:
-//   int widthOfBinaryTree(TreeNode* root) {
-//     long long maxRes = 0;
-//     queue<pair<TreeNode*, long long>> q;
-//     q.push({root,1});
-//     int l = 1;
-//     while(!q.empty())
-//     {
-//       long long qSize = q.size(), minQInd = INT_MAX, maxQInd = INT_MIN, count=0;
-//       while(qSize--)
-//       {
-//         TreeNode* node = q.front().first;
-//         long long currNodeVal = q.front().second;
-//         // cout<<"currNodeVal: "<<currNodeVal<<"\n";
-//         q.pop();
-//         minQInd = min(minQInd, currNodeVal);
-//         maxQInd = max(maxQInd, currNodeVal);
-//         if(node->left)
-//           q.push({node->left,(long long)2*currNodeVal-1});
-//         if(node->right)
-//           q.push({node->right,(long long)2*currNodeVal});
-//         count++;
-//       }
-//       cout<<"l: "<<l++<<" "<<maxQInd<<" "<<minQInd<<" "<<count<<" ";
-//       cout<<maxRes<<"\n";
-//       maxRes = max(maxRes, (maxQInd - minQInd + 1));
-//     }
-//     return maxRes;
-//   }
-// };
-
 class Solution {
 public:
-    int widthOfBinaryTree(TreeNode* root) {
-        int width = 0;
-        queue<pair<TreeNode*, int>>q;
-        q.push({root, 0});
-        while(!q.empty()){
-            int n = q.size();
-            int max_index = INT_MIN, min_index = INT_MAX;
-            while(n--){
-                TreeNode* tmp = q.front().first;
-                int dist = q.front().second;
-                q.pop();
-                max_index = max(max_index, dist);
-                min_index = min(min_index, dist);
-                if(tmp->left){
-                    q.push({tmp->left, (long long)2 * dist + 1});
-                }
-                if(tmp->right){
-                    q.push({tmp->right, (long long)2 * dist + 2});
-                }
-            }
-            width = max(width, max_index - min_index + 1);
-        }
-        return width;   
+  int widthOfBinaryTree(TreeNode* root) {
+    ull maxRes = 0;
+    queue<pair<TreeNode*, int>> q;
+    q.push({root,0});
+    ull l = 1;
+    while(!q.empty())
+    {
+      // int qSize = q.size(), minQInd = INT_MAX, maxQInd = INT_MIN, count=0;
+      long long qSize = q.size(), minQInd = LLONG_MAX, maxQInd = LLONG_MIN, count=0;
+      
+      while(qSize--)
+      {
+        TreeNode* node = q.front().first;
+        ull currNodeVal = q.front().second;
+        // cout<<"currNodeVal: "<<currNodeVal<<"\n";
+        q.pop();
+        minQInd = min(minQInd, currNodeVal);
+        maxQInd = max(maxQInd, currNodeVal);
+        if(node->left)
+          q.push({node->left, (long long)2 * currNodeVal + 1});
+        if(node->right)
+          q.push({node->right, (long long)2 * currNodeVal + 2});
+        count++;
+      }
+      // cout<<"l: "<<l++<<" "<<maxQInd<<" "<<minQInd<<" "<<count<<" ";
+      // cout<<maxRes<<"\n";
+      maxRes = max(maxRes, maxQInd - minQInd);
     }
+    return maxRes+1;
+  }
 };
+
+// class Solution {
+// public:
+//     int widthOfBinaryTree(TreeNode* root) {
+//         int width = 0;
+//         queue<pair<TreeNode*, int>>q;
+//         q.push({root, 0});
+//         while(!q.empty()){
+//             int n = q.size();
+//             int max_index = INT_MIN, min_index = INT_MAX;
+//             while(n--){
+//                 TreeNode* tmp = q.front().first;
+//                 int dist = q.front().second;
+//                 q.pop();
+//                 max_index = max(max_index, dist);
+//                 min_index = min(min_index, dist);
+//                 if(tmp->left){
+//                     q.push({tmp->left, (long long)2 * dist + 1});
+//                 }
+//                 if(tmp->right){
+//                     q.push({tmp->right, (long long)2 * dist + 2});
+//                 }
+//             }
+//             width = max(width, max_index - min_index + 1);
+//         }
+//         return width;   
+//     }
+// };
