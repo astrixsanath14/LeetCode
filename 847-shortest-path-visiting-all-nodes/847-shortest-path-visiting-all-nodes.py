8329bd340d45ec7a91bce5bc0e4a0e6eb7df4d45
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-27 19:13:41 +0530

    Time: 161 ms (97.77%), Space: 16.8 MB (85.45%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.py
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.py
@@ -1,46 +1,70 @@
-class Solution {
-    
-    public int shortestPathLength(int[][] graph) {
-        int n = graph.length;
-        int fullMask = (1 << n) - 1;
+from math import inf
+from collections import deque
+
+class Solution:
+    def shortestPathLength(self, graph):
+        # 1 <= graph.length <= 12
+        # 0 <= graph[i].length < graph.length
+
+        nodeCount = len(graph)
         
-        Set<String> visited = new HashSet<>();
-        Queue<Node> que = new LinkedList<>();
-        for (int i = 0; i < n; i++) {
-            Node node = new Node(i, 1<<i);
-            que.offer(node);
-            visited.add(node.toString());
-        }
-        
-        int level = 0;
-        while (!que.isEmpty()) {
-            int size = que.size();
-            for (int i = 0; i < size; i++) {
-                Node node = que.poll();
-                if (node.mask == fullMask) return level;
-                for (int next : graph[node.id]) {
-                    Node nextNode = new Node(next, node.mask | (1 << next));
-                    if (visited.contains(nextNode.toString())) continue;
-                    que.offer(nextNode);
-                    visited.add(nextNode.toString());
-                }
-            }
-            level++;
-        }
-        
-        return level;
-    }
-    
-    class Node {
-        int id;
-        int mask;
-        
-        Node(int id, int mask){
-            this.id = id; this.mask = mask;
-        }
-        
-        public String toString() {
-            return id + " " + mask;
-        }
-    }
-}
+        # NOTE
+        # We are using BFS here because it's better suited for 'shortest path'
+        # types of problems. DFS solution is also viable though.
+
+        # Thoughts:
+        # 1. start at each node, do BFS to try reaching all other nodes.
+        # 2. Must keep track of visited nodes, else infinite loop may happen.
+        # 3. But each node may have to be visited multiple times, as described in the problem
+        #    statement. So we cannot be too strict in limiting searches
+        # 4. We must describe the state during a search, we need:
+        #    - The current node we are on
+        #    - Nodes we have visited (Notice the order does not matter in this case, that's a key)
+
+        # each search is described by (currentNode, visited)
+        # same search does _not_ have to be repeated, since if re-visited with
+        # the same state, it would yield the same result.
+        # NOTE this does not prevent revisiting the same node again,
+        # it just prevents revisiting it with the same STATE!
+
+        # Since the input size is restricted, we can use a number to encode
+        # which nodes have been visited -- the i-th bit is on iff node i has been visited
+
+        # conceptually masks[k] indicates that only node k has been visited
+        masks = [1 << i for i in range(nodeCount)]
+        # used to check whether all nodes have been visited (11111...111)
+        allVisited = (1 << nodeCount) - 1
+        queue = deque([(i, masks[i]) for i in range(nodeCount)])
+        steps = 0
+
+        # encoded_visited in visited_states[node] iff
+        # (node, encoded_visited) has been pushed onto the queue
+        visited_states = [{masks[i]} for i in range(nodeCount)]
+        # states in visited_states will never be pushed onto queue again
+
+        while queue:
+            # number of nodes to be popped off for current steps size
+            # this avoids having to store steps along with the state
+            # which consumes both time and memory
+            count = len(queue)
+
+            while count:
+                currentNode, visited = queue.popleft()
+                if visited == allVisited:
+                    return steps
+
+                # start bfs from each neighbor
+                for nb in graph[currentNode]:
+                    new_visited = visited | masks[nb]
+                    # pre-check here to for efficiency, as each steps increment may results
+                    # in huge # of nodes being added into queue
+                    if new_visited == allVisited:
+                        return steps + 1
+                    if new_visited not in visited_states[nb]:
+                        visited_states[nb].add(new_visited)
+                        queue.append((nb, new_visited))
+
+                count -= 1
+            steps += 1
+        # no path which explores every node
+        return inf
