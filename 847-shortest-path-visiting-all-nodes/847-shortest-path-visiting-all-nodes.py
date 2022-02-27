4cb7d50dd18da23600812b995e1454e0b695bb35
astrixsanath14 <astrixsanath14@gmail.com>
2022-02-27 19:13:29 +0530

    Time: 233 ms (10.11%), Space: 75.5 MB (10.11%) - LeetHub

--- 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.py
+++ 847-shortest-path-visiting-all-nodes/847-shortest-path-visiting-all-nodes.py
@@ -1,26 +1,46 @@
-# DFS Top-Down DP Solution O(N^2 * 2^N) | O(N * 2^N)
-class Solution:
-    def shortestPathLength(self, graph: List[List[int]]) -> int:
-        cache = {}
-        def dfs(node, mask):
-            state = (node, mask)
-            # Base Case
-            if state in cache:
-                return cache[state]
-            if mask & (mask - 1) == 0:
-                return 0
-            # Init cache[state] to infinity to avoid endless loop between two nodes
-            cache[state] = float('inf')
-            for neighbor in graph[node]:
-                if mask & (1 << neighbor):
-                    # If already visited currentNode mask doesn't change
-                    visited = 1 + dfs(neighbor, mask)
-                    # If not visited currentNode set mask = mask ^ (1 << node)
-                    notVisited = 1 + dfs(neighbor, mask ^ (1 << node))
-                    cache[state] = min(cache[state], visited, notVisited)
-            return cache[state]
+class Solution {
+    
+    public int shortestPathLength(int[][] graph) {
+        int n = graph.length;
+        int fullMask = (1 << n) - 1;
         
-        endingMask = (1 << len(graph)) - 1
-        cache = {}
+        Set<String> visited = new HashSet<>();
+        Queue<Node> que = new LinkedList<>();
+        for (int i = 0; i < n; i++) {
+            Node node = new Node(i, 1<<i);
+            que.offer(node);
+            visited.add(node.toString());
+        }
         
-        return min(dfs(node, endingMask) for node in range(len(graph)))
+        int level = 0;
+        while (!que.isEmpty()) {
+            int size = que.size();
+            for (int i = 0; i < size; i++) {
+                Node node = que.poll();
+                if (node.mask == fullMask) return level;
+                for (int next : graph[node.id]) {
+                    Node nextNode = new Node(next, node.mask | (1 << next));
+                    if (visited.contains(nextNode.toString())) continue;
+                    que.offer(nextNode);
+                    visited.add(nextNode.toString());
+                }
+            }
+            level++;
+        }
+        
+        return level;
+    }
+    
+    class Node {
+        int id;
+        int mask;
+        
+        Node(int id, int mask){
+            this.id = id; this.mask = mask;
+        }
+        
+        public String toString() {
+            return id + " " + mask;
+        }
+    }
+}
