803f3ef86c0c3847ccf7853f5561d11956dda385
astrixsanath14 <astrixsanath14@gmail.com>
2022-03-07 07:34:07 +0530

    Time: 4 ms (94.94%), Space: 14.9 MB (45.55%) - LeetHub

--- 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
+++ 21-merge-two-sorted-lists/21-merge-two-sorted-lists.cpp
@@ -16,12 +16,12 @@ public:
     {
       if(list1->val < list2->val)
       {
-        temp->next = new ListNode(list1->val);
+        temp->next = list1;
         list1 = list1->next;
       }
       else
       {
-        temp->next = new ListNode(list2->val);
+        temp->next = list2;
         list2 = list2->next;
       }
       temp = temp->next;
