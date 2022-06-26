fbde68e4a6a620c085247dcdbd9128ce7eb288b2
astrixsanath14 <astrixsanath14@gmail.com>
2022-06-26 14:26:31 +0530

    Time: 7 ms (100.00%), Space: 39.1 MB (80.00%) - LeetHub

--- 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
+++ 2320-count-number-of-ways-to-place-houses/2320-count-number-of-ways-to-place-houses.java
@@ -1,15 +1,22 @@
-#define MOD (int)(1E9+7) 
 class Solution {
-public:
-    int countHousePlacements(int n) {
-      int houses = 1, spaces = 1;
-      long long total = houses + spaces;
-      for(int i=2;i<=n;i++)
-      {
-        houses = spaces;
-        spaces = total;
-        total = (houses + spaces)%MOD;
-      }
-      return (total*total)%MOD;
+  public int countHousePlacements(int n) {
+    int first = 1, second = 1, total = 0, mod = (int)1e9+7;
+    for(int i=1;i<=n;i++)
+    {
+      total = (first + second)%mod;
+      second = first;
+      first = total;
     }
-};
+    return (int)(1L * total * total % mod);
+  }
+}
+
+ // public int countHousePlacements(int n) {
+ //        int a = 1, b = 1, c = 2, mod = (int)1e9 + 7;
+ //        for (int i = 0; i < n; ++i) {
+ //            c = (a + b) % mod;
+ //            a = b;
+ //            b = c;
+ //        }
+ //        return (int)(1L * b * b % mod);
+ //    }
