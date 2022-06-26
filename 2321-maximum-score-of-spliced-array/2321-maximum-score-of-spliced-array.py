e600d28269a109bd3b91fdf8ed24e0b16c3d27b4
astrixsanath14 <astrixsanath14@gmail.com>
2022-06-26 14:51:19 +0530

    Time: 1904 ms (100.00%), Space: 32.4 MB (100.00%) - LeetHub

--- 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.py
+++ 2321-maximum-score-of-spliced-array/2321-maximum-score-of-spliced-array.py
@@ -1,29 +1,9 @@
-class Solution {
-public:
-  int maxSubArray(vector<int>& nums) {
-    int sum = 0, maxSum = INT_MIN;
-    for(int i = 0; i < nums.size(); i++)
-    {
-      sum += nums[i];
-      maxSum = max(maxSum, sum);
-      if(sum < 0)
-        sum = 0;
-    }
-    return maxSum;
-  }  
-  
-  int maximumsSplicedArray(vector<int>& nums1, vector<int>& nums2) {
-    int n = nums1.size(), sum1 = 0, sum2 = 0;
-    vector<int> aMinusB(n), bMinusA(n);
-    for(int i=0; i<n; i++)
-    {
-      sum1 += nums1[i];
-      sum2 += nums2[i];
-      
-      aMinusB[i] = nums1[i] - nums2[i];
-      bMinusA[i] = nums2[i] - nums1[i];
-    }
-    int ans = max(sum1, sum2);
-    return max(ans,max(sum1 + maxSubArray(bMinusA), sum2 + maxSubArray(aMinusB)));
-  }
-};
+class Solution:
+  def maximumsSplicedArray(self, A, B):
+    def kadane(A, B):
+        res = cur = 0
+        for i in range(len(A)):
+            cur = max(0, cur + A[i] - B[i])
+            res = max(res, cur)
+        return res + sum(B)
+    return max(kadane(A, B), kadane(B, A))
