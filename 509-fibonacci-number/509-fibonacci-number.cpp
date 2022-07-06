5dae163c67e6415b801445baa8ba5d3545c97056
astrixsanath14 <astrixsanath14@gmail.com>
2022-07-06 07:05:31 +0530

    Time: 30 ms (6.52%), Space: 5.7 MB (99.97%) - LeetHub

--- 509-fibonacci-number/509-fibonacci-number.cpp
+++ 509-fibonacci-number/509-fibonacci-number.cpp
@@ -1,11 +1,22 @@
+// class Solution {
+// public:
+//   map<int,int> m;
+//   int fib(int n) {
+//     if(n == 0 || n == 1)
+//     {
+//       return m[n] = n;
+//     }
+//     return m[n] = fib(n-1) + fib(n-2);
+//   }
+// };
+
 class Solution {
 public:
-  map<int,int> m;
   int fib(int n) {
     if(n == 0 || n == 1)
     {
-      return m[n] = n;
+      return n;
     }
-    return m[n] = fib(n-1) + fib(n-2);
+    return fib(n-1) + fib(n-2);
   }
 };
