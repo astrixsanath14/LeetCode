5563277327c89e03e6dbda0b7810c6c16779e320
astrixsanath14 <astrixsanath14@gmail.com>
2022-09-17 09:37:02 +0530

    Time: 575 ms (69.36%), Space: 54.8 MB (97.71%) - LeetHub

--- 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
+++ 1770-maximum-score-from-performing-multiplication-operations/1770-maximum-score-from-performing-multiplication-operations.cpp
@@ -55,46 +55,52 @@
 //   }
 // };
 
-// class Solution {
-// public:
-//     int m, n;
-//     int memo[1001][1001];
-//     int maximumScore(vector<int>& nums, vector<int>& muls) {
-//         n = nums.size();
-//         m = muls.size();
-//         memset(memo, INT_MIN, sizeof(memo));
-//         return dp(0, 0, nums, muls);
-//     }
-//     int dp(int l, int i, vector<int>& nums, vector<int>& muls) {
-//         if (i == m) return 0; // Picked enough m elements
-//         if (memo[l][i] != INT_MIN) return memo[l][i];
-//         int pickLeft = dp(l+1, i+1, nums, muls) + nums[l] * muls[i]; // Pick the left side
-//         int pickRight = dp(l, i+1, nums, muls) + nums[n-(i-l)-1] * muls[i]; // Pick the right side
-//         return memo[l][i] = max(pickLeft, pickRight);
-//     }
-// };
-
 class Solution {
 public:
-    vector<vector<int>> dp;
-    
-    int solve(int i, int n, int j, vector<int> &nums, vector<int> &M){
-        
-        if (j == M.size()) return 0;
-        if (dp[i][j] != INT_MIN) return dp[i][j];
-        
-        // Left Side
-        int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);
-        
-        // Right Side
-        int right = solve(i, n, j + 1, nums, M) + (nums[(n - 1) - (j - i)] * M[j]);
-        
-        return dp[i][j] = max(left, right);
+    int m, n;
+    int maximumScore(vector<int>& nums, vector<int>& muls) {
+      n = nums.size();
+      m = muls.size();
+      int memo[1001][1001];
+      for(int i=0;i<m;i++){
+        for(int j=0; j<m;j++){
+          memo[i][j] = INT_MIN;
+          // cout<<memo[i][j]<<" ";
+        }
+        // cout<<"\n";
+      }
+      return dp(0, 0, nums, muls, memo);
     }
-    
-    int maximumScore(vector<int>& nums, vector<int>& M) {   
-        int n = nums.size(), m = M.size();
-        dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
-        return solve(0, n, 0, nums, M);
+    int dp(int l, int i, vector<int>& nums, vector<int>& muls, int memo[][1001]) {
+        if (i == m) return 0; // Picked enough m elements
+        if (memo[l][i] != INT_MIN) return memo[l][i];
+        int pickLeft = dp(l+1, i+1, nums, muls, memo) + nums[l] * muls[i]; // Pick the left side
+        int pickRight = dp(l, i+1, nums, muls, memo) + nums[n-(i-l)-1] * muls[i]; // Pick the right side
+        return memo[l][i] = max(pickLeft, pickRight);
     }
 };
+
+// class Solution {
+// public:
+//     vector<vector<int>> dp;
+    
+//     int solve(int i, int n, int j, vector<int> &nums, vector<int> &M){
+        
+//         if (j == M.size()) return 0;
+//         if (dp[i][j] != INT_MIN) return dp[i][j];
+        
+//         // Left Side
+//         int left = solve(i + 1, n, j + 1, nums, M) + (nums[i] * M[j]);
+        
+//         // Right Side
+//         int right = solve(i, n, j + 1, nums, M) + (nums[(n - 1) - (j - i)] * M[j]);
+        
+//         return dp[i][j] = max(left, right);
+//     }
+    
+//     int maximumScore(vector<int>& nums, vector<int>& M) {   
+//         int n = nums.size(), m = M.size();
+//         dp.resize(m + 1, vector<int>(m + 1, INT_MIN));
+//         return solve(0, n, 0, nums, M);
+//     }
+// };
