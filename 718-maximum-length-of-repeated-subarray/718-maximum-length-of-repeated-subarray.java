e9874b64968366a2c23d10d9e9d54bbdd2d0137a
astrixsanath14 <astrixsanath14@gmail.com>
2022-09-20 09:00:18 +0530

    Time: 68 ms (73.19%), Space: 45.3 MB (91.78%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.java
@@ -1,44 +1,37 @@
+//Bottom - Up
 class Solution {
-public:
-  
-  int lors(vector<int>& nums1, vector<int>& nums2, int i, int j, vector<vector<int>>& dp)
+  public void printArr(int arr[])
   {
-    if(i == -1 || j == -1)
-    {
-      return 0;
-    }
-    if(dp[i][j] != -1)
-    {
-      return dp[i][j];
-    }
-    int res;
-    if(nums1[i] == nums2[j])
-    {
-      dp[i][j] = (1 + lors(nums1, nums2, i-1, j-1, dp));
-    }
-    else
-    {
-      dp[i][j] = 0;
-    }
-    
-    lors(nums1, nums2, i-1, j, dp);
-    lors(nums1, nums2, i, j-1, dp);
-    
-    return dp[i][j];
+    for(int i=0; i<arr.length; i++)
+      System.out.print(arr[i] + " ");
+    System.out.println();
   }
-  
-  int findLength(vector<int>& nums1, vector<int>& nums2) {
-    int m = nums1.size(), n = nums2.size();
-    vector<vector<int>> dp(m, vector<int>(n, -1));
-    lors(nums1, nums2, m-1, n-1, dp);
-    int result = -1;
-    for(int i = 0; i<m; i++)
+  public int findLength(int[] nums1, int[] nums2) {
+    int m = nums1.length;
+    int n = nums2.length;
+    int[] dp = new int[n];
+    int ans = 0;
+    for(int i=0; i<m; i++)
     {
-      for(int j=0;j<n;j++)
+      for(int j=n-1; j>=0; j--)
       {
-        result = max(result, dp[i][j]);
+        if(nums1[i] == nums2[j])
+        {
+          dp[j] = 1;
+          if(i > 0 && j > 0)
+          {
+            dp[j] = 1 + dp[j-1];
+          }
+        }
+        else
+        {
+          dp[j] = 0;
+        }
+        // System.out.println(i + " " + j);
+        // printArr(dp);
+        ans = Math.max(ans, dp[j]);
       }
     }
-    return result;
+    return ans;
   }
-};
+}
