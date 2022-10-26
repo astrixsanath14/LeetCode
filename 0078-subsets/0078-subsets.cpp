f2829d78c07dc9a400ba64f5168077630f05a77c
astrixsanath14 <astrixsanath14@gmail.com>
2022-10-26 07:33:53 +0530

    Time: 9 ms (21.97%), Space: 14 MB (37.98%) - LeetHub

---
+++ 0078-subsets/0078-subsets.cpp
@@ -0,0 +1,46 @@
+//Recursion
+class Solution {
+public:
+    void subsets(int i, vector<int>& nums, int j, vector<int> &tempSubset, vector<vector<int>> &result) {
+        if(i==nums.size())
+        {
+            vector<int> subset(j);
+            for(int t=0;t<j;t++)
+                subset[t] = tempSubset[t];
+            result.push_back(subset);
+            return;
+        }
+        subsets(i+1, nums, j, tempSubset, result);
+        tempSubset[j] = nums[i++];
+        subsets(i, nums, j+1, tempSubset, result);
+    }
+    
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<int> subset(nums.size());
+        vector<vector<int>> result;
+        subsets(0, nums, 0, subset, result);
+        return result;
+    }
+};
+
+
+// // Cascading technique
+// class Solution {
+// public:
+//     vector<vector<int>> subsets(vector<int>& nums) {
+//       int n = nums.size(), count=1;
+//       vector<vector<int>> ans(1);
+//       for(int num:nums)
+//       {
+//         vector<vector<int>> curNumCombo = ans;
+//         for(vector<int> curAns : ans)
+//         {
+//           vector<int> temp = curAns;
+//           temp.push_back(num);
+//           curNumCombo.push_back(temp);
+//         }
+//         ans = curNumCombo;
+//       }
+//       return ans;
+//     }
+// };
