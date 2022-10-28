d7900495ff2cb38644684efba92537f886a1cc07
astrixsanath14 <astrixsanath14@gmail.com>
2022-10-28 22:27:14 +0530

    Time: 85 ms (76.66%), Space: 42.3 MB (80.49%) - LeetHub

--- 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
+++ 1423-maximum-points-you-can-obtain-from-cards/1423-maximum-points-you-can-obtain-from-cards.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-  int maxScore(vector<int>& cardPoints, int k) {
-    long long maxPoints = 0, noOfCards = cardPoints.size(), prefixSum = 0;
-    vector<long long> suffixSum(noOfCards);
-    suffixSum[noOfCards-1] = cardPoints[noOfCards-1];
-    for(int i=noOfCards-2;i>=noOfCards-k;i--)
-      suffixSum[i] = cardPoints[i] + suffixSum[i+1];
-    maxPoints = suffixSum[noOfCards-k];
-    for(int i=0;i<k-1;i++)
-    {
-      prefixSum += cardPoints[i];
-      maxPoints = max(maxPoints, prefixSum + suffixSum[noOfCards-(k-(i+1))]);
+    int maxScore(vector<int>& cardPoints, int k) {
+        int sum = accumulate(cardPoints.begin(), cardPoints.end(), 0);
+        int minWindowSum = INT_MAX, windowSum = 0, n = cardPoints.size();
+        // cout<<"Sum: "<<sum<<"\n";
+        for(int i=0;i<n; i++)
+        {
+            windowSum += cardPoints[i];
+            if(i >= n-k)
+            {
+                windowSum -= cardPoints[i-(n-k)];
+                // cout<<windowSum<<"\n";
+                minWindowSum = min(minWindowSum, windowSum);
+            }
+            else
+                minWindowSum = windowSum;
+        }
+        return sum - (minWindowSum == INT_MAX ? 0 : minWindowSum);
     }
-    maxPoints = max(maxPoints, prefixSum + cardPoints[k-1]);
-    return maxPoints;
-  }
 };
