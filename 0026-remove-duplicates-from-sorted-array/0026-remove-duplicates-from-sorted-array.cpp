42511e9d38c8d0975bc5404848642ffd24b842e4
astrixsanath14 <astrixsanath14@gmail.com>
2022-11-11 08:49:48 +0530

    Time: 18 ms (77.30%), Space: 18.4 MB (37.30%) - LeetHub

---
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int removeDuplicates(vector<int>& nums) {
+        int pos = 1;
+        for(int i=1;i<nums.size();i++)
+            if(nums[i]!=nums[pos-1])
+                nums[pos++] = nums[i];
+        return pos;
+    }
+};
